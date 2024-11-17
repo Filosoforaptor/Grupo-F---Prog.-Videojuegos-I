fc5067b7370d8eefb25425d21e73e727a86e5c2d
Lopez Manuel <manuel.lopez.csta@gmail.com>
2024-11-17 01:38:08 -0300

    Juguetes se flipean random al crearse

--- juguete.js
+++ juguete.js
@@ -16,18 +16,24 @@ class Juguete extends Objeto {
 
       //   let height = texture.baseTexture.height;
       this.sprite = new PIXI.Sprite(texture);
-      
+
       //GUARDO EL RADIO DEL OBSTACULO PORQ LO USO PARA DETECTAR COLISIONES CON LOS PERSONAJES
       this.radio = this.sprite.width * 0.5;
       // this.radio=this.juego.grid.cellSize*0.5
       this.sprite.anchor.set(0.5, 0.82);
-
+      this.flipRandomly();
       this.container.addChild(this.sprite);
 
       this.actualizarZIndex();
       this.container.scale.x = -1;
       this.meterEnGrid();
     });
+  }
+
+  flipRandomly() {
+    if (Math.random() < 0.7) {
+      this.sprite.scale.x *= -1;
+    }
   }
 
   update() {
@@ -40,10 +46,10 @@ class Juguete extends Objeto {
       Math.cos(
         this.juego.contadorDeFrames * 0.04 * this.velocidadDeMovimientoRandom
       ) * 0.1;*/
-        const now = Date.now();
-        const speed = 0.001; // OJO CON TOCAR ESTAS VARIABLES e.e ES SUPER DIFICIL DE AFINAR e.e 
-        const amplitud = 0.1;
-      this.sprite.y += Math.sin(now * speed) * amplitud; 
+    const now = Date.now();
+    const speed = 0.001; // OJO CON TOCAR ESTAS VARIABLES e.e ES SUPER DIFICIL DE AFINAR e.e 
+    const amplitud = 0.1;
+    this.sprite.y += Math.sin(now * speed) * amplitud;
     super.update();
   }
 
